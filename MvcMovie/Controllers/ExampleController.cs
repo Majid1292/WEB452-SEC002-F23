2b7be5b967f3e1d77ad0627e9ca79d0f134690cb
Majid <49441325+Majid1292@users.noreply.github.com>
2023-09-20 14:01:02 -0400

    Add welcome page
    Using Controller and Views

--- MvcMovie/Controllers/ExampleController.cs
+++ MvcMovie/Controllers/ExampleController.cs
@@ -9,9 +9,11 @@ namespace MvcMovie.Controllers
             return View();
         }
 
-        public string Welcome(string name = "John ", int ID = 1)
+        public IActionResult Welcome(string name = "John", int num = 1)
         {
-            return "Your name is: " + name + " and Your ID is :" + ID;
+            ViewData["Message"] = "Hi " + name;
+            ViewData["Number"] = num;
+            return View();
         }
     }
 }
