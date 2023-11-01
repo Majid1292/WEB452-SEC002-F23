9a347385b66ce3f2ec17cf40c7e5f57eccb4dd81
Majid <49441325+Majid1292@users.noreply.github.com>
2023-11-01 14:16:42 -0400

    Upgrade validation rules

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,12 +12,15 @@ public class Movie
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set;}
     
-    
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "The field Genre must be started with capital letter")]
+    [StringLength(20)]
     public string Genre { get; set;}
 
     [Range(1,100)]
     [DataType(DataType.Currency)]
     public decimal Price { get; set;}
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    [StringLength(5)]
     public string Rating {get; set;}
 }
